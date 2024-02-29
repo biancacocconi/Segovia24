e1c36664d3da6ce84bc83a62e815235bfdf3054b
biancaauroracocconi <bcocconi.ieu2022@student.ie.edu>
2024-02-29 10:09:56 +0100

    practice

--- session 1.py
+++ session 1.py
@@ -1,2 +1,21 @@
 print("hello world again!")
 print("hello world again! this is a new change")
+print(5/5)
+print(5*5)
+print(5//2)
+print(17%10)
+print(2**10)
+# truth value testing
+
+name = input("what is your name? ")
+
+age = input("how old are you? ")
+age = int(age)
+birth_year = 2023 - age
+
+print(f"OMG {name}, you are {age} years old so you are born in {birth_year}!")
+
+age = int(input("what is your age? "))
+age = int(age)
+
+print(f"Your age is {age}")
---
+++ session 4.py
@@ -0,0 +1,14 @@
+f = open("x-files.txt", "w")
+while True:
+    line = input("give me some text to put into the file: ")
+    if line.lower() == "stop":
+        break
+    f.write(line + "\n")
+
+# we need to close the file at the end
+f.close()
+
+
+with open ("x-files.txt", "r") as f: #inside here the f file is open for reading
+    print(f.read()) # once i am out, the file is closed
+
